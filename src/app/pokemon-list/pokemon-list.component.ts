a84b0ec66ee20fc6ba4ab2d7e87fe2150e9c9f6d
Gabriel Leblond <120410801+gabriel-leblond@users.noreply.github.com>
2023-11-18 18:05:31 +0100

    ADD pokemon list component

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -2,15 +2,48 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Pokemon } from '../pokemon';
 import { POKEMONS } from '../mock-pokemon-list';
+import { BorderCardDirective } from '../border-card.directive';
+import { PokemonTypeColorPipe } from '../pokemon-type-color.pipe';
 
 
 @Component({
   selector: 'app-pokemon-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, BorderCardDirective, PokemonTypeColorPipe],
   templateUrl: './pokemon-list.component.html',
   styles: ``
 })
 export class PokemonListComponent {
   pokemonsList: Pokemon[] = POKEMONS;
+  pokemonSelected: Pokemon|undefined;
+
+  ngOnInit(): void {
+
+    console.log(this.pokemonsList);
+
+  }
+
+  selectPokemon(pokemonId: string){
+    const pokemon: Pokemon|undefined = this.pokemonsList.find( pokemon => pokemon.id == +pokemonId )
+
+    if (pokemon) {
+      this.pokemonSelected = pokemon;
+      console.log(`Tu as demander  ${pokemon.name}`);
+    } else {
+      this.pokemonSelected = pokemon;
+      console.log(`Le pokemon n'existe pas`)
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
 }
